434c7c618e318937c3823626f2a9377e22d1079f
Ivan Marinov <ivan@finite-soft.com>
2018-07-04 15:34:57 +0300

    References #4626 add 3 basic rights

--- app/RoleRight.php
+++ app/RoleRight.php
@@ -10,6 +10,28 @@ class RoleRight extends Model
     use RecordSignature;
 
     protected $guarded = ['id'];
+    const RIGHT_VIEW = 1;
+    const RIGHT_EDIT = 2;
+    const RIGHT_ALL = 3;
+
+    public static function getRights()
+    {
+        return [
+            self::RIGHT_VIEW    => 'View',
+            self::RIGHT_EDIT    => 'Edit',
+            self::RIGHT_ALL     => 'All',
+        ];
+    }
+
+    public static function getRightsDescription()
+    {
+        return [
+            self::RIGHT_VIEW    => 'View',
+            self::RIGHT_EDIT    => 'View and edit',
+            self::RIGHT_ALL     => 'View, edit and delete',
+        ];
+    }
+
 
     public function role()
     {

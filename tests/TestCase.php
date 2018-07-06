11db46e345fe05ce4d51f0451857b112a854b64e
Ivan Marinov <ivan@finite-soft.com>
2018-07-06 15:22:28 +0300

    References #4569 #4626 add unit tests for API

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function getApiKey()
+    {
+        return User::where('username', 'system')->first()->api_key;
+    }
 }

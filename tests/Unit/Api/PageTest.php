c8a4caed090c27b5926d3d0e993e04cb8174820e
Kaloyan Dimov <kaloyan@finite-soft.com>
2018-07-13 09:16:15 +0300

    Fixes #4571 added test

---
+++ tests/Unit/Api/PageTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class PageTest extends TestCase
+{
+    public function testList()
+    {
+        $response = $this->post(
+            url('api/pages/listPages'),
+            ['api_key' => $this->getApiKey()]
+        );
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(['success' => true]);
+    }
+}

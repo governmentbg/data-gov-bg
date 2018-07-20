bd5b88fa6e3c389763306d71ca7d13bb389a7245
Kaloyan Dimov <kaloyan@finite-soft.com>
2018-07-20 19:08:56 +0300

    remaining files

--- app/Document.php
+++ app/Document.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers\Traits\RecordSignature;
 
 class Document extends Model
 {
+    use RecordSignature;
+
     protected $guarded = ['id'];
 }

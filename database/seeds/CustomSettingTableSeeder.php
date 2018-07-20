7564a64798d4361e3e114b79d6d21703992d084f
Kaloyan Dimov <kaloyan@finite-soft.com>
2018-07-20 19:07:27 +0300

    Created tests and seeders for the remaining tables

---
+++ database/seeds/CustomSettingTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\DataSet;
+use App\Organisation;
+use App\CustomSetting;
+use App\Resource;
+
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class CustomSettingTableSeeder extends Seeder
+{
+    const CUSTOM_SETTING_RECORDS = 10;
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->faker = Faker::create();
+        $organisations = Organisation::select('id')->limit(self::CUSTOM_SETTING_RECORDS)->get()->toArray();
+
+        foreach (range(1, self::CUSTOM_SETTING_RECORDS) as $index) {
+            $organisation =  $this->faker->randomElement($organisations)['id'];
+
+            CustomSetting::create([
+                'org_id' => $organisation,
+                'data_set_id'  => null,
+                'resource_id' =>  null,
+                'key' => $this->faker->randomDigit(),
+                'value' => $this->faker->randomDigit()
+            ]);
+        }
+    }
+}
---
+++ database/seeds/DataSetSubCategoriesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\DataSet;
+use App\Category;
+use App\DataSetSubCategory;
+
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class DataSetSubCategoriesTableSeeder extends Seeder
+{
+    const DATA_SET_SUB_CATEGORIES_RECORDS = 10;
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->faker = Faker::create();
+        $dataSets = DataSet::select('id')->limit(self::DATA_SET_SUB_CATEGORIES_RECORDS)->get()->toArray();
+        $categories = Category::select('id')->limit(self::DATA_SET_SUB_CATEGORIES_RECORDS)->get()->toArray();
+
+        foreach (range(1, self::DATA_SET_SUB_CATEGORIES_RECORDS) as $index) {
+            $dataSet =  $this->faker->unique()->randomElement($dataSets)['id'];
+            $category = $this->faker->randomElement($categories)['id'];
+
+            DataSetSubCategory::create([
+                'data_set_id' => $dataSet,
+                'sub_cat_id'  => $category
+            ]);
+        }
+    }
+}

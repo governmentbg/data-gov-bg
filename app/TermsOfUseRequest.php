030f305229a5dea89f643e3f46cc9ba8fede2faa
Todor Mitovski <todor@finite-soft.com>
2018-07-20 17:53:59 +0300

    Fixes #4550 Api Terms of use requests

--- app/TermsOfUseRequest.php
+++ app/TermsOfUseRequest.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
+use App\Http\Controllers\Traits\RecordSignature;
 
 class TermsOfUseRequest extends Model
 {
+    use RecordSignature;
+    use Searchable;
+
     protected $guarded = ['id'];
+
+    const STATUS_NEW = 1;
+    const STATUS_PROCESSED = 2;
+
+    public function toSearchableArray()
+    {
+        $array['descript'] = $this->descript;
+        $array['id'] = $this->id;
+
+        return $array;
+    }
+
+    public function searchableAs()
+    {
+        return 'terms_of_use_requests';
+    }
 }

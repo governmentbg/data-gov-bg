48f4f5de7cebbb08d6138f5189ea6b10156b2b04
Kaloyan Dimov <kaloyan@finite-soft.com>
2018-07-31 15:13:23 +0300

    Fixes #4552 added api tests, comments in controller, fixed migration

--- app/Http/Controllers/Api/DataRequestController.php
+++ app/Http/Controllers/Api/DataRequestController.php
@@ -6,26 +6,41 @@ use App\DataRequest;
 use App\Http\Controllers\ApiController;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use \Validator;
 
 class DataRequestController extends ApiController
 {
+    /**
+     * Send a request based on input
+     *
+     * @param array data - required
+     * @param integer data[org_id] - required
+     * @param string data[description] - required
+     * @param string data[published_url] - optional
+     * @param string data[contact_name] - optional
+     * @param string data[email] - required
+     * @param string data[notes] - optional
+     * @param integer data[status] - optional
+     *
+     * @return response with request_id or error message
+     */
     public function sendDataRequest(Request $request)
     {
         $requestData = $request->all();
         $validator = Validator::make($requestData, [
-            'data' => 'required|array',
-            'data.org_id' => 'required|integer',
-            'data.description' => 'required|string',
-            'data.published_url' => 'string',
-            'data.contact_name' => 'string',
-            'data.email' => 'required|email',
-            'data.notes' => 'string',
-            'data.status' => 'integer',
+            'data'                  => 'required|array',
+            'data.org_id'           => 'required|integer',
+            'data.description'      => 'required|string',
+            'data.published_url'    => 'nullable|string',
+            'data.contact_name'     => 'nullable|string',
+            'data.email'            => 'required|email',
+            'data.notes'            => 'nullable|string',
+            'data.status'           => 'nullable|integer',
         ]);
 
         if ($validator->fails()) {
-            return $this->errorResponse('Send request failure');
+            return $this->errorResponse('Send request failure', $validator->errors()->messages());
         }
 
         $dataRequest = new DataRequest;
@@ -53,114 +68,146 @@ class DataRequestController extends ApiController
 
         try {
             $dataRequest->save();
+            return $this->successResponse(['request_id' => $dataRequest->id]);
         } catch (QueryException $e) {
-            return $this->errorResponse('Send request failure');
+            Log::error($ex->getMessage());
         }
-        return $this->successResponse(['request_id' => $dataRequest->id]);
-
     }
 
+     /**
+     * edit a request based on input
+     * @param integer request_id - required
+     * @param array data - required
+     * @param integer data[org_id] - optional
+     * @param string data[description] - optional
+     * @param string data[published_url] - optional
+     * @param string data[contact_name] - optional
+     * @param string data[email] - optional
+     * @param string data[notes] - optional
+     * @param integer data[status] - optional
+     *
+     * @return response with request_id or error message
+     */
     public function editDataRequest(Request $request)
     {
         $editRequestData = $request->all();
         $validator = Validator::make($editRequestData, [
-            'request_id' => 'required|integer',
-            'data' => 'required|array',
-            'data.org_id' => 'integer',
-            'data.description' => 'string',
-            'data.published_url' => 'string',
-            'data.contact_name' => 'string',
-            'data.email' => 'email',
-            'data.notes' => 'string',
-            'data.status' => 'integer',
+            'request_id'            => 'required|integer|exists:data_requests,id',
+            'data'                  => 'required|array',
+            'data.org_id'           => 'nullable|integer',
+            'data.description'      => 'nullable|string',
+            'data.published_url'    => 'nullable|string',
+            'data.contact_name'     => 'nullable|string',
+            'data.email'            => 'nullable|email',
+            'data.notes'            => 'nullable|string',
+            'data.status'           => 'nullable|integer',
         ]);
 
-        if ($validator->fails()) {
-            return $this->errorResponse('Edit data request failure');
-        }
-        $requestToEdit = DataRequest::find($editRequestData['request_id']);
+        if (!$validator->fails()) {
 
-        if (!$requestToEdit) {
-            return $this->errorResponse('Edit data request failure');
+            $requestToEdit = DataRequest::find($editRequestData['request_id']);
+
+            if (isset($editRequestData['data']['org_id'])) {
+                $requestToEdit->org_id = $editRequestData['data']['org_id'];
+            }
+
+            if (isset($editRequestData['data']['description'])) {
+                $requestToEdit->descript = $editRequestData['data']['description'];
+            }
+
+            if (isset($editRequestData['data']['published_url'])) {
+                $requestToEdit->published_url = $editRequestData['data']['published_url'];
+            }
+
+            if (isset($editRequestData['data']['contact_name'])) {
+                $requestToEdit->contact_name = $editRequestData['data']['contact_name'];
+            }
+
+            if (isset($editRequestData['data']['email'])) {
+                $requestToEdit->email = $editRequestData['data']['email'];
+            }
+
+            if (isset($editRequestData['data']['notes'])) {
+                $requestToEdit->notes = $editRequestData['data']['notes'];
+            }
+
+            if (isset($editRequestData['data']['status'])) {
+                $requestToEdit->status = $editRequestData['data']['status'];
+            }
+
+            try {
+                $requestToEdit->save();
+                return $this->successResponse();
+            } catch (QueryException $e) {
+                Log::error($ex->getMessage());
+            }
         }
 
-        if (isset($editRequestData['data']['org_id'])) {
-            $requestToEdit->org_id = $editRequestData['data']['org_id'];
-        }
-
-        if (isset($editRequestData['data']['description'])) {
-            $requestToEdit->descript = $editRequestData['data']['description'];
-        }
-
-        if (isset($editRequestData['data']['published_url'])) {
-            $requestToEdit->published_url = $editRequestData['data']['published_url'];
-        }
-
-        if (isset($editRequestData['data']['contact_name'])) {
-            $requestToEdit->contact_name = $editRequestData['data']['contact_name'];
-        }
-
-        if (isset($editRequestData['data']['email'])) {
-            $requestToEdit->email = $editRequestData['data']['email'];
-        }
-
-        if (isset($editRequestData['data']['notes'])) {
-            $requestToEdit->notes = $editRequestData['data']['notes'];
-        }
-
-        if (isset($editRequestData['data']['status'])) {
-            $requestToEdit->status = $editRequestData['data']['status'];
-        }
-
-        try {
-            $requestToEdit->save();
-        } catch (QueryException $e) {
-            return $this->errorResponse('Edit data request failure');
-        }
-        return $this->successResponse();
-
+    return $this->errorResponse('Edit data request failure', $validator->errors()->messages());
     }
 
+    /**
+     * Delete a data reuqest based on id
+     *
+     * @param integer request_id - required
+     *
+     * @return response with success or error message
+     */
     public function deleteDataRequest(Request $request)
     {
         $deleteRequestData = $request->all();
         $validator = Validator::make($deleteRequestData, [
-            'request_id' => 'required|integer',
+            'request_id' => 'required|integer|exists:data_requests,id',
         ]);
 
-        if ($validator->fails()) {
-            return $this->errorResponse('Delete data request failure');
-        }
+        if (!$validator->fails()) {
 
-        $requestToDelete = DataRequest::find($deleteRequestData['request_id']);
+            $requestToDelete = DataRequest::find($deleteRequestData['request_id']);
 
-        if (!$requestToDelete) {
-            return $this->errorResponse('Delete data request failure');
+            try {
+                $requestToDelete->delete();
+                return $this->successResponse();
+            } catch (QueryException $e) {
+
+            }
         }
-        try {
-            $requestToDelete->delete();
-        } catch (QueryException $e) {
-            return $this->errorResponse('Delete data request failure');
-        }
-        return $this->successResponse();
+        return $this->errorResponse('Delete data request failure', $validator->errors()->messages());
     }
 
+    /**
+     * Delete a data reuqest based on id
+     *
+     * @param integer request_id - optional
+     * @param array criteria - optional
+     * @param integer criteria[org_id] - optional
+     * @param integer criteria[status] - optional
+     * @param date criteria[date_from] - optional
+     * @param date criteria[date_to] - optional
+     * @param string criteria[search] - optional
+     * @param array criteria[order] - optional
+     * @param string criteria[order][type] - optional
+     * @param string criteria[order][field] - optional
+     * @param integer records_per_page - optional
+     * @param integer page_number - optional
+     *
+     * @return response with success or error message
+     */
     public function listDataRequests(Request $request)
     {
         $listRequestData = $request->all();
         $validator = Validator::make($listRequestData, [
-            'criteria' => 'array',
-            'criteria.request_id' => 'integer',
-            'criteria.org_id' => 'integer',
-            'criteria.status' => 'integer',
-            'criteria.date_from' => 'date',
-            'criteria.date_to' => 'date',
-            'criteria.search' => 'string',
-            'criteria.order' => 'array',
-            'criteria.order.type' => 'string',
-            'criteria.order.field' => 'string',
-            'records_per_page' => 'integer',
-            'page_number' => 'integer',
+            'criteria'                => 'nullable|array',
+            'criteria.request_id'     => 'nullable|integer',
+            'criteria.org_id'         => 'nullable|integer',
+            'criteria.status'         => 'nullable|integer',
+            'criteria.date_from'      => 'nullable|date',
+            'criteria.date_to'        => 'nullable|date',
+            'criteria.search'         => 'nullable|string',
+            'criteria.order'          => 'nullable|array',
+            'criteria.order.type'     => 'nullable|string',
+            'criteria.order.field'    => 'nullable|string',
+            'records_per_page'        => 'nullable|integer',
+            'page_number'             => 'nullable|integer',
         ]);
 
         if ($validator->fails()) {
@@ -193,10 +240,6 @@ class DataRequestController extends ApiController
             }
         }
 
-        if (is_null($criteria)) {
-            $dataRequestList = $dataRequestList;
-        }
-
         if (isset($criteria['request_id'])) {
             $dataRequestList = $dataRequestList->where('request_id', $criteria['request_id']);
         }
@@ -208,6 +251,8 @@ class DataRequestController extends ApiController
         if (isset($criteria['status'])) {
             $dataRequestList = $dataRequestList->where('status', $criteria['status']);
         }
+
+        $total_records = $dataRequestList->count();
 
         if (isset($request['records_per_page']) && isset($request['page_number'])) {
             $dataRequestList = $dataRequestList->forPage($request->input('page_number'), $request->input('records_per_page'));
@@ -243,27 +288,26 @@ class DataRequestController extends ApiController
         $dataRequestList = $dataRequestList->get();
 
         if (!empty($dataRequestList)) {
-            $total_records = $dataRequestList->count();
             foreach ($dataRequestList as $singleDataRequest) {
                 $result[] = [
-                    'id' => $singleDataRequest->id,
-                    'org_id' => $singleDataRequest->org_id,
-                    'description' => $singleDataRequest->descript,
-                    'published_url' => $singleDataRequest->published_url,
-                    'contact_name' => $singleDataRequest->contact_name,
-                    'email' => $singleDataRequest->email,
-                    'notes' => $singleDataRequest->notes,
-                    'status' => $singleDataRequest->status,
-                    'created_at' => date($singleDataRequest->created_at),
-                    'updated_at' => date($singleDataRequest->updated_at),
-                    'created_by' => $singleDataRequest->created_by,
-                    'updated_by' => $singleDataRequest->updated_by,
+                    'id'               => $singleDataRequest->id,
+                    'org_id'           => $singleDataRequest->org_id,
+                    'description'      => $singleDataRequest->descript,
+                    'published_url'    => $singleDataRequest->published_url,
+                    'contact_name'     => $singleDataRequest->contact_name,
+                    'email'            => $singleDataRequest->email,
+                    'notes'            => $singleDataRequest->notes,
+                    'status'           => $singleDataRequest->status,
+                    'created_at'       => date($singleDataRequest->created_at),
+                    'updated_at'       => date($singleDataRequest->updated_at),
+                    'created_by'       => $singleDataRequest->created_by,
+                    'updated_by'       => $singleDataRequest->updated_by,
                 ];
             }
         }
         return $this->successResponse([
             'total_records' => $total_records,
-            'dataRequests' => $result,
+            'dataRequests'  => $result,
         ], true);
 
     }
--- tests/Unit/Api/DataRequestTest.php
+++ tests/Unit/Api/DataRequestTest.php
@@ -2,13 +2,184 @@
 
 namespace Tests\Unit\Api;
 
+use App\DataRequest;
 use Tests\TestCase;
+use App\Organisation;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class DataRequestTest extends TestCase
 {
-   
+    use WithFaker;
+    use DatabaseTransactions;
+
+    public function testSendDataRequest()
+    {
+        //test missing api key
+        $this->post(url('api/sendDataRequest'), ['api_key' => null])
+            ->assertStatus(403)
+            ->assertJson(['success' => false]);
+
+        //test missing data
+        $this->post(
+            url('api/sendDataRequest'),
+            [
+                'api_key'   => $this->getApiKey(),
+                'data'      => [],
+            ]
+        )
+            ->assertStatus(500)
+            ->assertJson(['success' => false]);
+        $count = DataRequest::all()->count();
+        $orgs = Organisation::orderBy('created_at', 'desc')->limit(10)->get()->toArray();
+        $org = $this->faker->randomElement($orgs)['id'];
+
+        $this->post(
+            url('api/sendDataRequest'),
+            [
+                'api_key'   => $this->getApiKey(),
+                'data'      => [
+                    'org_id'           => $org,
+                    'description'      => $this->faker->sentence(3),
+                    'published_url'    => $this->faker->url,
+                    'contact_name'     => $this->faker->name,
+                    'email'            => $this->faker->email,
+                    'notes'            => $this->faker->sentence(4),
+                    'status'           => 1
+                ],
+            ]
+        )
+            ->assertStatus(200)
+            ->assertJson(['success' => true]);
+        // check that a record is made
+        $this->assertTrue($count + 1 == DataRequest::all()->count());
+    }
+
+    public function testEditDataRequest()
+    {
+        $orgs = Organisation::orderBy('created_at', 'desc')->limit(10)->get()->toArray();
+        $org = $this->faker->randomElement($orgs)['id'];
+
+        $request = DataRequest::create([
+                    'org_id'           => $org,
+                    'descript'         => $this->faker->sentence(3),
+                    'published_url'    => $this->faker->url,
+                    'contact_name'     => $this->faker->name,
+                    'email'            => $this->faker->email,
+                    'notes'            => $this->faker->sentence(4),
+                    'status'           => $this->faker->boolean()
+        ]);
+
+        //test missing api key
+        $this->post(url('api/editDataRequest'), ['api_key' => null])
+            ->assertStatus(403)
+            ->assertJson(['success' => false]);
+
+        //test missing request id
+        $this->post(
+            url('api/editDataRequest'),
+            [
+                'api_key'   => $this->getApiKey(),
+                'request_id'   => null,
+                'data'      => [
+                    'org_id'           => $org,
+                    'description'      => $this->faker->sentence(3),
+                    'published_url'    => $this->faker->url,
+                    'contact_name'     => $this->faker->name,
+                    'email'            => $this->faker->email,
+                    'notes'            => $this->faker->sentence(4),
+                    'status'           => $this->faker->boolean()
+                ],
+            ]
+        )
+            ->assertStatus(500)
+            ->assertJson(['success' => false]);
+
+        //test missing data
+        $this->post(
+            url('api/editDataRequest'),
+            [
+                'api_key'      => $this->getApiKey(),
+                'request_id'   => $request->id,
+                'data'         => [],
+            ]
+        )
+            ->assertStatus(500)
+            ->assertJson(['success' => false]);
+
+        // test successful testEdit
+        $this->post(
+            url('api/editDataRequest'),
+            [
+                'api_key'        => $this->getApiKey(),
+                'request_id'     => $request->id,
+                'data'           => [
+                    'org_id'           => $org,
+                    'description'      => $this->faker->sentence(3),
+                    'published_url'    => $this->faker->url,
+                    'contact_name'     => $this->faker->name,
+                    'email'            => $this->faker->email,
+                    'notes'            => $this->faker->sentence(4),
+                    'status'           => 1
+                ],
+            ]
+        )
+            ->assertStatus(200)
+            ->assertJson(['success' => true]);
+    }
+
+    public function testDeleteDataRequest()
+    {
+        $orgs = Organisation::orderBy('created_at', 'desc')->limit(10)->get()->toArray();
+        $org = $this->faker->randomElement($orgs)['id'];
+
+        $request = DataRequest::create([
+                    'org_id'           => $org,
+                    'descript'         => $this->faker->sentence(3),
+                    'published_url'    => $this->faker->url,
+                    'contact_name'     => $this->faker->name,
+                    'email'            => $this->faker->email,
+                    'notes'            => $this->faker->sentence(4),
+                    'status'           => 1
+        ]);
+
+        //test missing api key
+        $this->post(url('api/deleteDataRequest'),
+            [
+                'api_key' => null,
+                'request_id' => $request->id
+            ]
+        )
+            ->assertStatus(403)
+            ->assertJson(['success' => false]);
+
+        //test missing request id
+        $this->post(
+            url('api/deleteDataRequest'),
+            [
+                'api_key'     => $this->getApiKey(),
+                'request_id'   => null,
+            ]
+        )
+            ->assertStatus(500)
+            ->assertJson(['success' => false]);
+
+        $count = DataRequest::all()->count();
+
+        //test successful delete
+        $this->post(
+            url('api/deleteDataRequest'),
+            [
+                'api_key'   => $this->getApiKey(),
+                'request_id'   => $request->id,
+            ]
+        )
+            ->assertStatus(200)
+            ->assertJson(['success' => true]);
+        // check that a record is missing
+        $this->assertTrue($count - 1 == DataRequest::all()->count());
+    }
+
     /**
      * A basic test example.
      *
@@ -20,10 +191,9 @@ class DataRequestTest extends TestCase
             url('api/listDataRequests'),
             ['api_key' => $this->getApiKey()]
         );
+
         $response
             ->assertStatus(200)
             ->assertJson(['success' => true]);
     }
-        
-    
 }

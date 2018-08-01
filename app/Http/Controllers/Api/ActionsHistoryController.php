5c56aa610f9cf50d9c7f34f9779a88e75a206aeb
Kaloyan Dimov <kaloyan@finite-soft.com>
2018-08-01 11:20:05 +0300

    Fixes #4625 fix error message

--- app/Http/Controllers/Api/ActionsHistoryController.php
+++ app/Http/Controllers/Api/ActionsHistoryController.php
@@ -176,7 +176,7 @@ class ActionsHistoryController extends ApiController
             'actions_history'   => $results,
         ], true);
     }
-    
+
     /**
      * Lists modules from ActionsHistory model
      *
@@ -186,14 +186,15 @@ class ActionsHistoryController extends ApiController
     public function listModules(Request $request)
     {
         $modules = ActionsHistory::getModuleNames();
- 
+
         if (!empty($modules)) {
             foreach ($modules as $module) {
                 $result[] = ['name' => $module];
             }
-            return $this->successResponse(['modules'=>$result],true); 
-        }
-        return $this->errorResponse();
-    }
 
+            return $this->successResponse(['modules'=>$result],true);
+        }
+
+        return $this->errorResponse('Get data failure');
+    }
 }

93637320585beed0493a637ffea19a6ad0dd624f
Ivan Marinov <ivan@finite-soft.com>
2018-08-10 13:27:18 +0300

    Fixes #4868 edit group image bug

--- resources/views/user/groupDatasets.blade.php
+++ resources/views/user/groupDatasets.blade.php
@@ -25,45 +25,51 @@
             <div class="row">
                 <div class="col-xs-12">
                     <div class="articles m-t-lg">
-                        @foreach ($datasets as $set)
-                            <div class="article m-b-lg col-xs-12 user-dataset">
-                                <div>{{ __('custom.date_added') }}: {{ $set->created_at }}</div>
-                                <div class="col-sm-12 p-l-none">
-                                    <a href="{{ url('/user/groups/dataset/view/'. $set->uri) }}">
-                                        <h2 class="m-t-xs">{{ $set->name }}</h2>
-                                    </a>
-                                    <div class="desc">
-                                        {{ $set->descript }}
-                                    </div>
-                                    <div class="col-sm-12 p-l-none btns">
-                                        <div class="pull-left row">
-                                            <div class="col-xs-6">
-                                                <span class="badge badge-pill m-r-md m-b-sm">
-                                                    <a href="{{ url('/user/groups/dataset/edit/'. $set->uri) }}">{{ __('custom.edit') }}</a>
-                                                </span>
-                                            </div>
-                                            <div class="col-xs-6">
-                                                <form method="POST">
-                                                    {{ csrf_field() }}
-                                                    <div class="col-xs-6 text-right">
-                                                        <button
-                                                            class="badge badge-pill m-b-sm"
-                                                            type="submit"
-                                                            name="delete"
-                                                            onclick="return confirm('Изтриване на данните?');"
-                                                        >{{ __('custom.remove') }}</button>
-                                                    </div>
-                                                    <input type="hidden" name="dataset_uri" value="{{ $set->uri }}">
-                                                </form>
-                                            </div>
+                        @if (count($datasets))
+                            @foreach ($datasets as $set)
+                                <div class="article m-b-lg col-xs-12 user-dataset">
+                                    <div>{{ __('custom.date_added') }}: {{ $set->created_at }}</div>
+                                    <div class="col-sm-12 p-l-none">
+                                        <a href="{{ url('/user/groups/dataset/view/'. $set->uri) }}">
+                                            <h2 class="m-t-xs">{{ $set->name }}</h2>
+                                        </a>
+                                        <div class="desc">
+                                            {{ $set->descript }}
                                         </div>
-                                        <div class="pull-right">
-                                            <span><a href="{{ route('datasetView', ['uri' => $set->uri]) }}">{{ __('custom.see_more') }}</a></span>
+                                        <div class="col-sm-12 p-l-none btns">
+                                            <div class="pull-left row">
+                                                <div class="col-xs-6">
+                                                    <span class="badge badge-pill m-r-md m-b-sm">
+                                                        <a href="{{ url('/user/groups/dataset/edit/'. $set->uri) }}">{{ __('custom.edit') }}</a>
+                                                    </span>
+                                                </div>
+                                                <div class="col-xs-6">
+                                                    <form method="POST">
+                                                        {{ csrf_field() }}
+                                                        <div class="col-xs-6 text-right">
+                                                            <button
+                                                                class="badge badge-pill m-b-sm"
+                                                                type="submit"
+                                                                name="delete"
+                                                                onclick="return confirm('Изтриване на данните?');"
+                                                            >{{ __('custom.remove') }}</button>
+                                                        </div>
+                                                        <input type="hidden" name="dataset_uri" value="{{ $set->uri }}">
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            <div class="pull-right">
+                                                <span><a href="{{ route('datasetView', ['uri' => $set->uri]) }}">{{ __('custom.see_more') }}</a></span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                            @endforeach
+                        @else
+                            <div class="col-sm-12 m-t-xl text-center">
+                                {{ __('custom.no_info') }}
                             </div>
-                        @endforeach
+                        @endif
                     </div>
                 </div>
             </div>

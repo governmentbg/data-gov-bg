a1ec865c2517337f2fe77218404bdf07762d1814
Kaloyan Dimov <kaloyan@finite-soft.com>
2018-08-14 08:05:43 +0300

    Fixes mail template and mail text

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -1,34 +1,7 @@
-<div
-    style="
-        text-align: center;
-        margin-right: 350px;
-    "
->
-    <a
-        href="{{ url('/') }}"
-        style="margin-right: 15px;"
-    ><img src="{{ $message->embed('img/op-logo.png') }}"></a>
-    <a
-        href="https://europa.eu/european-union/index_bg"
-    ><img src="{{ $message->embed('img/eu-logo.png') }}"></a>
-    <a
-        href="{{ url('/') }}"
-        style="margin-left: 8px;"
-    ><img src="{{ $message->embed('img/opdu-logo.png') }}"></a>
-    <br/>
-</div>
-<hr
-    style="
-        margin-left: 30%;
-        margin-right: 30%;
-        text-align: center;
-    "
->
 <table
     style="
         width: 100%;
-        height: 700px;
-        background: url({{ url('img/watermark-logo.png') }});
+        height: 1000px;
         background-repeat: no-repeat;
         background-position: center center;
     "
@@ -41,9 +14,52 @@
                     padding-left: 30%;
                 "
             >
-                <p>
-                    @yield('content')
+                <a
+                    href="{{ url('/') }}"
+                    style="margin-right: 15px"
+                ><img src="{{ $message->embed('img/op-logo.png') }}"></a>
+                <a
+                    href="https://europa.eu/european-union/index_bg"
+                ><img src="{{ $message->embed('img/eu-logo.png') }}"></a>
+                <a
+                    href="{{ url('/') }}"
+                    style="margin-left: 8px;"
+                ><img src="{{ $message->embed('img/opdu-logo.png') }}"></a><br>
+                <p
+                    style="
+                        font-family: 'Roboto', sans-serif;
+                        font-size:16px;
+                    "
+                >
+                    @yield('title')
                 </p>
+                <hr
+                    style="
+                        margin-top: 2em;
+                        margin-right: 40%;
+                        text-align: center;
+                        margin-bottom: 2em;
+                    "
+                >
+                <p
+                    style="
+                            font-family: 'Roboto', sans-serif;
+                            font-size: 16px;
+                    "
+                >
+                    @yield('content')
+                </p><br>
+                <div
+                    style="
+                        width: 100%;
+                        height: 800px;
+                        background: url({{ asset('img/watermark.png') }});
+                        background-repeat: no-repeat;
+                        background-position: left top;
+                        background-size: 60%;
+                    "
+                >
+                </div>
             </td>
         </tr>
     </tbody>

1f64ec8bc2419dc2f8dbcfb829d422605d650ba9
Kaloyan Dimov <kaloyan@finite-soft.com>
2018-08-14 08:05:43 +0300

    Fixes mail template and mail text

--- resources/views/mail/confirmationMail.blade.php
+++ resources/views/mail/confirmationMail.blade.php
@@ -1,8 +1,15 @@
 @extends('layouts.mail')
+
+@section('title')
+
+<b>{{__('custom.register_confirmation')}}</b>
+
+@endsection
+
 @section('content')
 
-{{ __('custom.greetings') }}, {{ $user }}!
-{{ __('custom.register_success') }}.
+{{ __('custom.greetings') }}, {{ $user }}!<br>
+{{ __('custom.register_success') }}.<br>
 {{ __('custom.to_activate') }}:<br/>
 <a href="{{ route('confirmation', ['hash' => $hash]) }}"> {{ __('custom.confirm') }}</a>
 

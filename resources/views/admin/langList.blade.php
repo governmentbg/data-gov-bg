07d54e636b6cf483e04a2903ba532239971bb2ba
Ivan Marinov <ivan@finite-soft.com>
2018-08-20 14:38:33 +0300

    Fix htlm style

--- resources/views/admin/langList.blade.php
+++ resources/views/admin/langList.blade.php
@@ -4,17 +4,15 @@
 <div class="container admin">
     @include('partials.alerts-bar')
     @include('partials.admin-nav-bar', ['view' => 'languages'])
-    <div class="row">
-        <h3>{{ __('custom.language_list') }}</h3>
-    </div>
+    <h3>{{ __('custom.language_list') }}</h3>
 
     <div class="row">
         <form method="POST" class="form-horizontal">
             {{ csrf_field() }}
-            <div class="row">
-                <a class="btn btn-primary pull-right add" href="{{ url('/admin/languages/add') }}">{{ __('custom.add') }}</a>
+            <div class="col-lg-12">
+                <a class="btn btn-primary pull-right add" href="{{ url('/admin/languages/add') }}">{{ ultrans('custom.add') }}</a>
             </div>
-            <div class="row">
+            <div class="col-lg-12">
                 <div class="table-responsive opn-tbl text-center">
                     <table class="table">
                         <thead>
@@ -33,24 +31,16 @@
                                         <a
                                             class="link-action"
                                             href="{{ url('/admin/languages/delete/'. $lang->locale) }}"
-                                            onclick="return confirm('Изтриване на данните?');"
+                                            data-confirm="Изтриване на данните?"
                                         >{{ __('custom.delete') }}</a>
                                         <a
                                             class="link-action"
-                                            href="{{ url('/admin/languages/edit/'. $lang->locale)}}"
+                                            href="{{ url('/admin/languages/edit/'. $lang->locale) }}"
                                         >{{ utrans('custom.edit') }}</a>
-                                        <a
+                                        <button
                                             class="link-action"
                                             href="#"
-                                        >{{ utrans('custom.export') }}</a>
-                                        <a
-                                            class="link-action"
-                                            href="#"
-                                        >{{ utrans('custom.import') }}</a>
-                                        <a
-                                            class="link-action"
-                                            href="#"
-                                        >{{ utrans('custom.preview') }}</a>
+                                        >{{ utrans('custom.preview') }}</button>
                                     </td>
                                 </tr>
                                 <input type="hidden" name="id" value="{{ $lang->locale }}">

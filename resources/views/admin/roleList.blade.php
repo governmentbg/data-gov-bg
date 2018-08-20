1c7a69867abc2dac13e8b18126dcdfdef11f81da
Dimitar Delchev <dimitar@finite-soft.com>
2018-08-20 12:05:53 +0300

    Fix admin panel table styles

--- resources/views/admin/roleList.blade.php
+++ resources/views/admin/roleList.blade.php
@@ -4,39 +4,36 @@
 <div class="container role">
     @include('partials.alerts-bar')
     @include('partials.admin-nav-bar', ['view' => 'manageRoles'])
-    <div class="row">
-        <h3>{{ __('custom.role_list') }}</h3>
-    </div>
+    <h3>{{ __('custom.role_list') }}</h3>
 
     <div class="row">
         <form method="POST" class="form-horizontal">
             {{ csrf_field() }}
-            <div class="row">
-                <a class="btn btn-primary pull-right add" href="{{ url('/admin/roles/add') }}">{{ __('custom.add') }}</a>
+            <div class="col-lg-12">
+                <a class="btn btn-primary pull-right add" href="{{ url('/admin/roles/add') }}">{{ ultrans('custom.add') }}</a>
             </div>
-            <div class="row">
+            <div class="col-lg-12">
                 <div class="table-responsive opn-tbl text-center">
                     <table class="table">
                         <thead>
                             <th>{{ __('custom.role_name') }}</th>
-                        <th>{{ __('custom.activity') }}</th>
+                            <th>{{ __('custom.activity') }}</th>
                             <th>{{ __('custom.by_default') }}</th>
                             <th>{{ __('custom.action') }}</th>
                         </thead>
                         <tbody>
                             @foreach ($roles as $role)
-                            <tr>
-                                <td>{{ $role->name }}</td>
-                                <td>{{ $role->active ? __('custom.active') : __('custom.not_active') }}</td>
-                                <td>не разбирам</td>
-                                <td>
-                                    <div class="inline-block">
+                                <tr>
+                                    <td>{{ $role->name }}</td>
+                                    <td>{{ $role->active ? __('custom.active') : __('custom.not_active') }}</td>
+                                    <td></td>
+                                    <td class="buttons">
                                         <button
                                             class="link-action"
                                             type="submit"
                                             name="delete"
-                                            onclick="return confirm('Изтриване на данните?');"
-                                            >{{ __('custom.delete') }}</button>
+                                            data-confirm="Изтриване на данните?"
+                                        >{{ __('custom.delete') }}</button>
                                         <a
                                             class="link-action"
                                             href="{{ url('/admin/roles/edit/'. $role->id) }}"
@@ -45,10 +42,9 @@
                                             class="link-action"
                                             href="#"
                                         >{{ utrans('custom.preview') }}</a>
-                                    </div>
-                                </td>
-                            </tr>
-                            <input type="hidden" name="id" value="{{ $role->id }}">
+                                    </td>
+                                </tr>
+                                <input type="hidden" name="id" value="{{ $role->id }}">
                             @endforeach
                         </tbody>
                     </table>

c319f7bab6f38b5e22a324a25a154ae448a467b8
Ivan Marinov <ivan@finite-soft.com>
2018-08-24 09:57:28 +0300

    Fixes #4398 Fixes #4400 add users pages for admin panel

--- app/Http/Controllers/AdminContoller.php
+++ app/Http/Controllers/AdminContoller.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\OrganisationController as ApiOrganisation;
+
 class AdminController extends Controller
 {
+    public function getOrgDropdown($userId = null)
+    {
+        $request = Request::create('/api/listOrganisations', 'POST', ['criteria' => ['user_id' => $userId]]);
+        $api = new ApiOrganisation($request);
+        $result = $api->listOrganisations($request)->getData();
+        $organisations = [];
 
+        foreach ($result->organisations as $row) {
+            $organisations[$row->id] = $row->name;
+        }
+
+        return $organisations;
+    }
 }

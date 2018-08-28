309052b65d867d872f25356a2d0c01ff3d08b009
Kaloyan Dimov <kaloyan@finite-soft.com>
2018-08-28 16:41:09 +0300

    Fixes #4980 added multiple select to orgs-groups user menus

--- resources/views/admin/addOrgMembersNew.blade.php
+++ resources/views/admin/addOrgMembersNew.blade.php
@@ -104,10 +104,11 @@
                         </div>
                         <div class="form-group row">
                             <label for="newsLetter" class="col-sm-3 col-xs-12 col-form-label">{{ __('custom.roles') }}:</label>
-                            <div class="col-sm-3 col-xs-6 p-r-none">
+                            <div class="col-sm-9">
                                 <select
-                                    class="js-select form-control open-select"
-                                    name="role_id"
+                                    multiple="multiple"
+                                    class="js-select form-control"
+                                    name="role_id[]"
                                     data-placeholder="{{ __('custom.select_role') }}"
                                     size="5"
                                 >
@@ -120,13 +121,13 @@
                                     @endforeach
                                 </select>
                             </div>
-                            <div class="col-sm-6 col-xs-6 text-right p-l-none reg-btns">
-                                <button
-                                    type="submit"
-                                    name="save"
-                                    class="m-l-md btn btn-primary m-b-sm"
-                                >{{ uctrans('custom.save') }}</button>
-                            </div>
+                        </div>
+                        <div class="reg-btns">
+                            <button
+                                type="submit"
+                                name="save"
+                                class="m-l-md btn pull-right btn-primary m-b-sm"
+                            >{{ uctrans('custom.save') }}</button>
                         </div>
                     </form>
                 </div>

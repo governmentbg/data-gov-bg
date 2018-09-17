4904fe693172bca94a7782441b71305f6f9c560a
Dimitar Delchev <dimitar@finite-soft.com>
2018-09-17 16:54:15 +0300

    Add initial languages in locale table, send mail test command

---
+++ app/Console/Commands/email.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class email extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:send {to}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send mail to recipient';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $to = $this->argument('to');
+
+        Mail::send('contact/contact', ['class' => 'user'], function ($m) use ($to) {
+            $m->from(env('MAIL_FROM', 'no-reply@finite-soft.com'), env('APP_NAME'));
+            $m->to($to, 'test user');
+            $m->subject('Test mail');
+        });
+
+        $this->info('Mail sent');
+    }
+}

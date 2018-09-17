a35207b13c3b03ebf678aaa1eb2ef3c190a66853
Dimitar Delchev <dimitar@finite-soft.com>
2018-09-17 16:54:15 +0300

    Add initial languages in locale table, send mail test command

---
+++ database/migrations/2018_09_07_152326_insert_base_languages_in_locale.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Locale;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class InsertBaseLanguagesInLocale extends Migration
+{
+    public function __construct()
+    {
+        $this->languages = [
+            'bg'    => true,
+            'en'    => true,
+        ];
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Locale::all()->count()) {
+            foreach ($this->languages as $language => $active) {
+                Locale::create([
+                    'locale'    => $language,
+                    'active'    => $active,
+                ]);
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach ($this->languages as $language) {
+            if (!Locale::where('locale', $language)->count()) {
+                Locale::where(['locale' => $language['name']])->get()->delete();
+            }
+        }
+    }
+}

61b407ca43e12ceaeb0b9ca502f3a77d6c1c699f
Denis Tsatsarov <denis@finite-soft.com>
2018-09-17 09:04:13 +0300

    References #4444. Remove admin role check.

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -77,53 +77,49 @@ class PageController extends AdminController
      */
     public function list(Request $request)
     {
-        if (Role::isAdmin()) {
-            $perPage = 10;
-            $params = [
-                'api_key'          => \Auth::user()->api_key,
-                'records_per_page' => $perPage,
-                'page_number'      => !empty($request->page) ? $request->page : 1,
-            ];
+        $perPage = 10;
+        $params = [
+            'api_key'          => \Auth::user()->api_key,
+            'records_per_page' => $perPage,
+            'page_number'      => !empty($request->page) ? $request->page : 1,
+        ];
 
 
-            $sections = Section::whereExists(function ($query) {
-                    $query->select()
-                        ->from('pages')
-                        ->whereRaw('sections.id = pages.section_id');
-                })
-                ->where('parent_id', null)->get();
+        $sections = Section::whereExists(function ($query) {
+                $query->select()
+                    ->from('pages')
+                    ->whereRaw('sections.id = pages.section_id');
+            })
+            ->where('parent_id', null)->get();
 
-            $sections = $this->prepareSections($sections);
+        $sections = $this->prepareSections($sections);
 
-            if (isset($request->active)) {
-                $params['criteria']['active'] = $request->active;
-            }
-
-            if (isset($request->section)) {
-                $params['criteria']['section_id'] = $request->section;
-            }
-
-            $req = Request::create('/api/listPages', 'POST', $params);
-            $api = new ApiPage($req);
-            $result = $api->listPages($req)->getData();
-            $getParams = array_except(app('request')->input(), ['page']);
-
-            $paginationData = $this->getPaginationData(
-                $result->pages,
-                $result->total_records,
-                $getParams,
-                $perPage
-            );
-
-            return view('/admin/pages', [
-                'class'         => 'user',
-                'pages'         => $paginationData['items'],
-                'pagination'    => $paginationData['paginate'],
-                'sections'      => $sections,
-            ]);
+        if (isset($request->active)) {
+            $params['criteria']['active'] = $request->active;
         }
 
-        return redirect()->back()->with('alert-danger', __('custom.access_denied_page'));
+        if (isset($request->section)) {
+            $params['criteria']['section_id'] = $request->section;
+        }
+
+        $req = Request::create('/api/listPages', 'POST', $params);
+        $api = new ApiPage($req);
+        $result = $api->listPages($req)->getData();
+        $getParams = array_except(app('request')->input(), ['page']);
+
+        $paginationData = $this->getPaginationData(
+            $result->pages,
+            $result->total_records,
+            $getParams,
+            $perPage
+        );
+
+        return view('/admin/pages', [
+            'class'         => 'user',
+            'pages'         => $paginationData['items'],
+            'pagination'    => $paginationData['paginate'],
+            'sections'      => $sections,
+        ]);
     }
 
     /**
@@ -136,30 +132,27 @@ class PageController extends AdminController
      */
     public function view(Request $request, $id)
     {
-        if (Role::isAdmin()) {
-            $req = Request::create('/api/listPages', 'POST', ['criteria' => ['page_id' => $id]]);
-            $api = new ApiPage($req);
-            $result = $api->listPages($req)->getData();
-            $page = isset($result->pages[0]) ? $result->pages[0] : null;
 
-            if (!is_null($page)) {
-                $section = Section::where('id', $page->section_id)->where('parent_id', null)->value('name');
-                $page = $this->getModelUsernames($page);
+        $req = Request::create('/api/listPages', 'POST', ['criteria' => ['page_id' => $id]]);
+        $api = new ApiPage($req);
+        $result = $api->listPages($req)->getData();
+        $page = isset($result->pages[0]) ? $result->pages[0] : null;
 
-                return view(
-                    'admin/pagesView',
-                    [
-                        'class'   => 'user',
-                        'page'    => $page,
-                        'section' => $section,
-                    ]
-                );
-            }
+        if (!is_null($page)) {
+            $section = Section::where('id', $page->section_id)->where('parent_id', null)->value('name');
+            $page = $this->getModelUsernames($page);
 
-            return redirect('/admin/pages/list');
+            return view(
+                'admin/pagesView',
+                [
+                    'class'   => 'user',
+                    'page'    => $page,
+                    'section' => $section,
+                ]
+            );
         }
 
-        return redirect()->back()->with('alert-danger', __('custom.access_denied_page'));
+        return redirect('/admin/pages/list');
     }
 
     /**
@@ -171,27 +164,79 @@ class PageController extends AdminController
      */
     public function edit(Request $request, $id)
     {
-        if (Role::isAdmin()) {
-            $class = 'user';
-            $fields = self::getPageTransFields();
-            $model = Page::find($id);
-            $sections = [];
 
+        $class = 'user';
+        $fields = self::getPageTransFields();
+        $model = Page::find($id);
+        $sections = [];
 
-            if (!is_null($model)) {
-                $model = $this->getModelUsernames($model->loadTranslations());
-                $model->valid_from = date_create($model->valid_from)
-                    ? date_format(date_create($model->valid_from), 'd-m-Y')
-                    : $model->valid_from;
-                $model->valid_to = date_create($model->valid_to)
-                    ? date_format(date_create($model->valid_to), 'd-m-Y')
-                    : $model->valid_to;
+        if (!is_null($model)) {
+            $model = $this->getModelUsernames($model->loadTranslations());
+            $model->valid_from = date_create($model->valid_from)
+                ? date_format(date_create($model->valid_from), 'd-m-Y')
+                : $model->valid_from;
+            $model->valid_to = date_create($model->valid_to)
+                ? date_format(date_create($model->valid_to), 'd-m-Y')
+                : $model->valid_to;
 
-                $sections = Section::where('parent_id', null)->get();
-                $sections = $this->prepareSections($sections);
+            $sections = Section::where('parent_id', null)->get();
+            $sections = $this->prepareSections($sections);
+        } else {
+            return redirect('admin/pages/list');
+        }
+
+        $from = null;
+        $to = null;
+
+        if (date_create($request->offsetGet('valid_from'))) {
+            $from = date_format(date_create($request->offsetGet('valid_from')), 'Y-m-d H:i:s');
+        }
+
+        if (date_create($request->offsetGet('valid_to'))) {
+            $to = date_format(date_create($request->offsetGet('valid_to')), 'Y-m-d H:i:s');
+        }
+
+        if ($request->has('edit')) {
+            $rq = Request::create('/api/editPage', 'POST', [
+                'page_id' => $id,
+                'data' => [
+                    'title'            => $request->offsetGet('title'),
+                    'section_id'       => $request->offsetGet('section_id'),
+                    'body'             => $request->offsetGet('body'),
+                    'head_title'       => $request->offsetGet('head_title'),
+                    'meta_description' => $request->offsetGet('meta_descript'),
+                    'meta_keywords'    => $request->offsetGet('meta_key_words'),
+                    'abstract'         => $request->offsetGet('abstract'),
+                    'forum_link'       => $request->offsetGet('forum_link'),
+                    'active'           => !empty($request->offsetGet('active')),
+                    'valid_from'       => $from,
+                    'valid_to'         => $to,
+                ]
+            ]);
+
+            $api = new ApiPage($rq);
+            $result = $api->editPage($rq)->getData();
+
+            if ($result->success) {
+                $request->session()->flash('alert-success', __('custom.edit_success'));
+
+                return back();
             } else {
-                return redirect('admin/pages/list');
+                $request->session()->flash('alert-danger', __('custom.edit_error'));
+
+                return back()->withErrors(isset($result->errors) ? $result->errors : []);
             }
+        }
+
+        return view('admin/pagesEdit', compact('class', 'fields', 'model', 'sections'));
+    }
+
+    public function add(Request $request)
+    {
+        $sections = Section::where('parent_id', null)->get();
+        $sections = $this->prepareSections($sections);
+
+        if ($request->has('create')) {
 
             $from = null;
             $to = null;
@@ -204,99 +249,39 @@ class PageController extends AdminController
                 $to = date_format(date_create($request->offsetGet('valid_to')), 'Y-m-d H:i:s');
             }
 
-            if ($request->has('edit')) {
-                $rq = Request::create('/api/editPage', 'POST', [
-                    'page_id' => $id,
-                    'data' => [
-                        'title'            => $request->offsetGet('title'),
-                        'section_id'       => $request->offsetGet('section_id'),
-                        'body'             => $request->offsetGet('body'),
-                        'head_title'       => $request->offsetGet('head_title'),
-                        'meta_description' => $request->offsetGet('meta_descript'),
-                        'meta_keywords'    => $request->offsetGet('meta_key_words'),
-                        'abstract'         => $request->offsetGet('abstract'),
-                        'forum_link'       => $request->offsetGet('forum_link'),
-                        'active'           => !empty($request->offsetGet('active')),
-                        'valid_from'       => $from,
-                        'valid_to'         => $to,
-                    ]
-                ]);
+            $rq = Request::create('/api/addPage', 'POST', [
+                'data' => [
+                    'title'            => $request->offsetGet('title'),
+                    'section_id'       => $request->offsetGet('section_id'),
+                    'body'             => $request->offsetGet('body'),
+                    'head_title'       => $request->offsetGet('head_title'),
+                    'meta_description' => $request->offsetGet('meta_descript'),
+                    'meta_keywords'    => $request->offsetGet('meta_key_words'),
+                    'abstract'         => $request->offsetGet('abstract'),
+                    'forum_link'       => $request->offsetGet('forum_link'),
+                    'active'           => !empty($request->offsetGet('active')),
+                    'valid_from'       => $from,
+                    'valid_to'         => $to,
+                ]
+            ]);
+            $api = new ApiPage($rq);
+            $result = $api->addPage($rq)->getData();
 
-                $api = new ApiPage($rq);
-                $result = $api->editPage($rq)->getData();
+            if (!empty($result->success)) {
+                $request->session()->flash('alert-success', __('custom.add_success'));
 
-                if ($result->success) {
-                    $request->session()->flash('alert-success', __('custom.edit_success'));
+                return redirect('/admin/pages/view/'. $result->data->page_id);
+            } else {
+                $request->session()->flash('alert-danger', __('custom.add_error'));
 
-                    return back();
-                } else {
-                    $request->session()->flash('alert-danger', __('custom.edit_error'));
-
-                    return back()->withErrors(isset($result->errors) ? $result->errors : []);
-                }
+                return back()->withErrors($result->errors)->withInput(Input::all());
             }
-
-            return view('admin/pagesEdit', compact('class', 'fields', 'model', 'sections'));
         }
 
-        return redirect()->back()->with('alert-danger', __('custom.access_denied_page'));
-    }
-
-    public function add(Request $request)
-    {
-        if (Role::isAdmin()) {
-            $sections = Section::where('parent_id', null)->get();
-            $sections = $this->prepareSections($sections);
-
-            if ($request->has('create')) {
-
-                $from = null;
-                $to = null;
-
-                if (date_create($request->offsetGet('valid_from'))) {
-                    $from = date_format(date_create($request->offsetGet('valid_from')), 'Y-m-d H:i:s');
-                }
-
-                if (date_create($request->offsetGet('valid_to'))) {
-                    $to = date_format(date_create($request->offsetGet('valid_to')), 'Y-m-d H:i:s');
-                }
-
-                $rq = Request::create('/api/addPage', 'POST', [
-                    'data' => [
-                        'title'            => $request->offsetGet('title'),
-                        'section_id'       => $request->offsetGet('section_id'),
-                        'body'             => $request->offsetGet('body'),
-                        'head_title'       => $request->offsetGet('head_title'),
-                        'meta_description' => $request->offsetGet('meta_descript'),
-                        'meta_keywords'    => $request->offsetGet('meta_key_words'),
-                        'abstract'         => $request->offsetGet('abstract'),
-                        'forum_link'       => $request->offsetGet('forum_link'),
-                        'active'           => !empty($request->offsetGet('active')),
-                        'valid_from'       => $from,
-                        'valid_to'         => $to,
-                    ]
-                ]);
-                $api = new ApiPage($rq);
-                $result = $api->addPage($rq)->getData();
-
-                if (!empty($result->success)) {
-                    $request->session()->flash('alert-success', __('custom.add_success'));
-
-                    return redirect('/admin/pages/view/'. $result->data->page_id);
-                } else {
-                    $request->session()->flash('alert-danger', __('custom.add_error'));
-
-                    return back()->withErrors($result->errors)->withInput(Input::all());
-                }
-            }
-
-            return view(
-                'admin/pagesAdd',
-                ['class' => 'user', 'fields' => self::getPageTransFields(), 'sections' => $sections]
-            );
-        }
-
-        return redirect()->back()->with('alert-danger', __('custom.access_denied_page'));
+        return view(
+            'admin/pagesAdd',
+            ['class' => 'user', 'fields' => self::getPageTransFields(), 'sections' => $sections]
+        );
     }
 
     /**
@@ -308,28 +293,23 @@ class PageController extends AdminController
      */
     public function delete(Request $request, $id)
     {
-        if (Role::isAdmin()) {
-            $class = 'user';
+        $rq = Request::create('/api/deletePage', 'POST', [
+            'page_id' => $id,
+        ]);
 
-            $rq = Request::create('/api/deletePage', 'POST', [
-                'page_id' => $id,
-            ]);
+        $api = new ApiPage($rq);
+        $result = $api->deletePage($rq)->getData();
 
-            $api = new ApiPage($rq);
-            $result = $api->deletePage($rq)->getData();
+        if ($result->success) {
+            $request->session()->flash('alert-success', __('custom.delete_success'));
 
-            if ($result->success) {
-                $request->session()->flash('alert-success', __('custom.delete_success'));
+            return redirect('/admin/pages/list');
+        } else {
+            $request->session()->flash('alert-danger', __('custom.delete_error'));
 
-                return redirect('/admin/pages/list');
-            } else {
-                $request->session()->flash('alert-danger', __('custom.delete_error'));
-
-                return redirect('/admin/pages/list')->withErrors(isset($result->errors) ? $result->errors : []);
-            }
+            return redirect('/admin/pages/list')->withErrors(isset($result->errors) ? $result->errors : []);
         }
 
-        return redirect()->back()->with('alert-danger', __('custom.access_denied_page'));
     }
 
     /**

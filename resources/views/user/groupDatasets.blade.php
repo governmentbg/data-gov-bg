14fea273f55bc2fc4a5b167941422bf66bb4a6cc
Ivan Marinov <ivan@finite-soft.com>
2018-09-18 12:35:17 +0300

    References #4495 fix resourse creation and view components

--- resources/views/user/groupDatasets.blade.php
+++ resources/views/user/groupDatasets.blade.php
@@ -4,13 +4,22 @@
 <div class="container">
     @include('partials.alerts-bar')
     @include('partials.user-nav-bar', ['view' => 'group'])
+    @include('partials.group-nav-bar', ['view' => 'datasets', 'group' => $group])
     @include('partials.pagination')
     <div class="row">
-    @if ($buttons['add'])
-        <div class="col-sm-3 col-xs-12 text-left">
-            <span class="badge badge-pill m-t-lg new-data user-add-btn"><a href="{{ url('/user/groups/dataset/create') }}">{{ __('custom.add_new_dataset') }}</a></span>
+        <div class="col-sm-3 col-xs-12 text-left sidenav">
+        @if ($buttons['add'])
+            <span class="badge badge-pill m-t-lg new-data user-add-btn"><a href="{{ url('/user/groups/dataset/create/'. $uri) }}">{{ __('custom.add_new_dataset') }}</a></span>
+        @endif
+            <div class="org m-t-lg">
+                <img src="{{ $group->logo }}">
+                <h2>{{ $group->name }}</h2>
+                <h4>{{ !empty($group->descript) ? truncate($group->descript, 150) : '' }}</h4>
+                <p class="text-right show-more">
+                    <a href="{{ url('/user/groups/view/'. $group->uri) }}" class="view-profile">{{ __('custom.see_more') }}</a>
+                </p>
+            </div>
         </div>
-    @endif
         <div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 search-field">
             <form method="GET" action="{{ url('/user/organisations/search') }}">
                 <input
@@ -22,63 +31,57 @@
                 >
             </form>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12 m-t-md">
-            <div class="row">
-                <div class="col-xs-12">
-                    <div class="articles m-t-lg">
-                        @if (count($datasets))
-                            @foreach ($datasets as $set)
-                                <div class="article m-b-lg col-xs-12 user-dataset">
-                                    <div>{{ __('custom.date_added') }}: {{ $set->created_at }}</div>
-                                    <div class="col-sm-12 p-l-none">
-                                        <a href="{{ route('groupDatasetView', $set->uri) }}">
-                                            <h2 class="m-t-xs">{{ $set->name }}</h2>
-                                        </a>
-                                        <div class="desc">
-                                            {{ $set->descript }}
+        <div class="col-lg-9 col-xs-12 m-t-md">
+            <div class="articles m-t-lg">
+                @if (count($datasets))
+                    @foreach ($datasets as $set)
+                        <div class="article m-b-lg col-xs-12 user-dataset">
+                            <div>{{ __('custom.date_added') }}: {{ $set->created_at }}</div>
+                            <div class="col-sm-12 p-l-none">
+                                <a href="{{ route('groupDatasetView', $set->uri) }}">
+                                    <h2 class="m-t-xs">{{ $set->name }}</h2>
+                                </a>
+                                <div class="desc">
+                                    {{ $set->descript }}
+                                </div>
+                                <div class="col-sm-12 p-l-none btns">
+                                    <div class="pull-left row">
+                                        <div class="col-xs-6">
+                                        @if ($buttons[$set->uri]['edit'])
+                                            <span class="badge badge-pill m-r-md m-b-sm">
+                                                <a href="{{ url('/user/groups/dataset/edit/'. $set->uri) }}">{{ uctrans('custom.edit') }}</a>
+                                            </span>
+                                        @endif
                                         </div>
-                                        <div class="col-sm-12 p-l-none btns">
-                                            <div class="pull-left row">
-                                                <div class="col-xs-6">
-                                                @if ($buttons[$set->uri]['edit'])
-                                                    <span class="badge badge-pill m-r-md m-b-sm">
-                                                        <a href="{{ url('/user/groups/dataset/edit/'. $set->uri) }}">{{ uctrans('custom.edit') }}</a>
-                                                    </span>
-                                                @endif
-                                                </div>
-                                                @if ($buttons[$set->uri]['delete'])
-                                                    <div class="col-xs-6">
-                                                        <form method="POST">
-                                                            {{ csrf_field() }}
-                                                            <div class="col-xs-6 text-right">
-                                                                <button
-                                                                    class="badge badge-pill m-b-sm del-btn"
-                                                                    type="submit"
-                                                                    name="delete"
-                                                                    onclick="return confirm('Изтриване на данните?');"
-                                                                >{{ uctrans('custom.remove') }}</button>
-                                                            </div>
-                                                            <input type="hidden" name="dataset_uri" value="{{ $set->uri }}">
-                                                        </form>
+                                        @if ($buttons[$set->uri]['delete'])
+                                            <div class="col-xs-6">
+                                                <form method="POST">
+                                                    {{ csrf_field() }}
+                                                    <div class="col-xs-6 text-right">
+                                                        <button
+                                                            class="badge badge-pill m-b-sm del-btn"
+                                                            type="submit"
+                                                            name="delete"
+                                                            onclick="return confirm('Изтриване на данните?');"
+                                                        >{{ uctrans('custom.remove') }}</button>
                                                     </div>
-                                                @endif
+                                                    <input type="hidden" name="dataset_uri" value="{{ $set->uri }}">
+                                                </form>
                                             </div>
-                                            <div class="pull-right">
-                                                <span><a href="{{ route('datasetView', ['uri' => $set->uri]) }}">{{ __('custom.see_more') }}</a></span>
-                                            </div>
-                                        </div>
+                                        @endif
+                                    </div>
+                                    <div class="pull-right">
+                                        <span><a href="{{ route('datasetView', ['uri' => $set->uri]) }}">{{ __('custom.see_more') }}</a></span>
                                     </div>
                                 </div>
-                            @endforeach
-                        @else
-                            <div class="col-sm-12 m-t-xl text-center no-info">
-                                {{ __('custom.no_info') }}
                             </div>
-                        @endif
+                        </div>
+                    @endforeach
+                @else
+                    <div class="col-sm-12 m-t-xl text-center no-info">
+                        {{ __('custom.no_info') }}
                     </div>
-                </div>
+                @endif
             </div>
         </div>
     </div>

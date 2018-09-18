bf3c26b0787a6d352d50ea9f8214f84e5b06fcf6
Todor Mitovski <todor@finite-soft.com>
2018-09-18 16:34:58 +0300

    Merge iframe page fix

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -370,6 +370,7 @@ $(function() {
     if ($('#js-code').length > 0) {
         var width = $("#js-width").val();
         var height = $("#js-height").val();
+        var uri = $(".js-res-uri").data('uri');
 
         $("#js-width").on("change paste keyup", function(width, height) {
             width = $(this).val();
@@ -392,8 +393,7 @@ $(function() {
 
         function updateTextarea(width, height) {
             var code = '<iframe width="'+ width +'" height="'+ height;
-            code += '" src="'+ window.location.href +'"';
-            code += '></iframe>';
+            code += '" src="'+ window.location.origin +'/data/resource/embed/'+ uri +'"></iframe>';
             $('#js-code').val(code);
         }
     }

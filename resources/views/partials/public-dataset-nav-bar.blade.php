ffc61423a197d78d8647e2c46eef8a8dc8ac261d
Antonia Pepeliankova <antonia@finite-soft.com>
2018-09-18 15:32:16 +0300

    Fixes #4455 - Dataset View

---
+++ resources/views/partials/public-dataset-nav-bar.blade.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-sm-9 col-xs-11 p-sm col-sm-offset-3">
+        <div class="filter-content">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-xs-12 p-l-r-none">
+                        <ul class="nav filter-type right-border">
+                            <li><a class="active p-l-none" href="{{ url('/data') }}">{{ __('custom.data') }}</a></li>
+                            <li><a href="{{ url('/data/linkedData') }}">{{ __('custom.linked_data') }}</a></li>
+                            <li><a href="{{ url('/data/reported') }}">{{ __('custom.signal_data') }}</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

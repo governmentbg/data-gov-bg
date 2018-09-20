445aa2c0bfccfd6d8e4e4d1f08f31dd45f2f84c6
Dimitar Delchev <dimitar@finite-soft.com>
2018-09-20 19:57:06 +0300

    Fix missing classes for data migrations

--- app/Http/Controllers/Api/ResourceController.php
+++ app/Http/Controllers/Api/ResourceController.php
@@ -2,17 +2,18 @@
 namespace App\Http\Controllers\Api;
 
 use Uuid;
+use App\Module;
+use App\Signal;
 use App\DataSet;
 use App\Resource;
-use App\Signal;
-use App\Module;
 use App\RoleRight;
+use App\Organisation;
 use App\ActionsHistory;
 use App\ElasticDataSet;
-use App\Organisation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ApiController;
 use Illuminate\Database\QueryException;
 

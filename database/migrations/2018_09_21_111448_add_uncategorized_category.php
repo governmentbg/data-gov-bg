7bea6d31c71697a5abcc1c788d12fee1beb3f538
Reni Todorova <reni@finite-soft.com>
2018-09-21 15:57:34 +0300

    Pick category from tags; add category for datasets with no tags; improve down migration

---
+++ database/migrations/2018_09_21_111448_add_uncategorized_category.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUncategorizedCategory extends Migration
+{
+    public function __construct()
+    {
+        $this->categories = [
+            [
+                'name'              => [
+                    'bg'                => 'Некатегоризирани',
+                    'en'                => 'Uncategorized',
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $currCount = Category::count();
+        foreach ($this->categories as $catData) {
+            $catData['active'] = true;
+            $catData['ordering'] = $currCount + 1;
+            $catData['icon_mime_type'] = 'image/svg+xml';
+
+            if (!Category::where($catData)->count()) {
+                Category::create($catData);
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach ($this->categories as $catData) {
+            if (Category::where($catData)->count()) {
+                Category::where($catData)->get()->delete();
+            }
+        }
+    }
+}

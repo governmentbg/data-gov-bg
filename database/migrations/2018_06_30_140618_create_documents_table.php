c830a801765e2d507218cd2a23e6dadd944e0f7e
Dimitar Delchev <dimitar@finite-soft.com>
2018-10-01 13:36:32 +0300

    Fix unresolved migration conflict

--- database/migrations/2018_06_30_140618_create_documents_table.php
+++ database/migrations/2018_06_30_140618_create_documents_table.php
@@ -13,20 +13,6 @@ class CreateDocumentsTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD
-        Schema::create('documents', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('name')->unsigned();
-            $table->integer('descript')->unsigned();
-            $table->string('file_name');
-            $table->string('mime_type');
-            $table->timestamps();
-            $table->integer('updated_by')->unsigned()->nullable();
-            $table->foreign('updated_by')->references('id')->on('users');
-            $table->integer('created_by')->unsigned();
-            $table->foreign('created_by')->references('id')->on('users');
-        });
-=======
         if (!env('IS_TOOL')) {
             Schema::create('documents', function (Blueprint $table) {
                 $table->increments('id');
@@ -41,10 +27,7 @@ class CreateDocumentsTable extends Migration
                 $table->integer('created_by')->unsigned();
                 $table->foreign('created_by')->references('id')->on('users');
             });
-
-            DB::statement("ALTER TABLE documents ADD data LONGBLOB NOT NULL");
         }
->>>>>>> 3566345b2a2f036b0f7e9c087e8abc4f93dda7eb
     }
 
     /**

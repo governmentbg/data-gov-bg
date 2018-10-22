35752eb5077e84af3aea0d56dd64e65c3c2deb67
Kaloyan Dimov <kaloyan@finite-soft.com>
2018-10-22 15:57:57 +0300

    Fixes #4468 Added forum for news

--- resources/views/news/view.blade.php
+++ resources/views/news/view.blade.php
@@ -28,23 +28,11 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- IF there are commnets -->
-                        <!-- <div class="col-sm-12 pull-left m-t-md p-l-none">
-                            <div class="comments p-lg">
-                                @for ($i=0; $i<3; $i++)
-                                    <div class="comment-box p-lg m-b-lg">
-                                        <img class="img-rounded coment-avatar" src="{{ asset('img/test-img/avatar.png') }}"/>
-                                        <p class="comment-author p-b-xs">{{ __('custom.profile_name') }}</p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                            sed do eiusmod tempor incididunt ut labore et dolore magna
-                                            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                            ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        </p>
-                                    </div>
-                                @endfor
+                        @if (isset($discussion))
+                            <div class="row discussion">
+                                @include('vendor.chatter.discussion')
                             </div>
-                        </div> -->
+                        @endif
                     </div>
                 </div>
             </div>

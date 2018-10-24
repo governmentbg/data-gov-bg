22b358c25505f8a2b419208a16b141914a30a4b2
Todor Mitovski <todor@finite-soft.com>
2018-10-24 13:36:11 +0300

    Fixes #5389 On login set locale from user settings

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -78,6 +78,8 @@ class LoginController extends Controller
                 if (Auth::attempt($credentials, $rememberMe)) {
                     $user = User::where('username', $request->username)->first();
                     $result = User::getUserRoles($user->id);
+                    $settings = $user->userSetting;
+                    Session::put('locale', $settings->locale);
 
                     Session::put('roles', $result);
 

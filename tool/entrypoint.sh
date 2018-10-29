d31d903c1c6176e459b97d759b92149a7be3566a
Dimitar Delchev <dimitar@finite-soft.com>
2018-10-29 13:40:03 +0200

    References #4518 Tool instalation and docker files

---
+++ tool/entrypoint.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# start apache
+httpd
+
+# start cron
+crond
+
+if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+fi
+
+if [ -d /var/lib/mysql/mysql ]; then
+    echo '[i] MySQL directory already present, skipping creation'
+else
+    echo "[i] CRON initializing jobs..."
+    echo "*/30 * * * * cd /var/www/localhost/htdocs/ && source deploy.sh master" > /mnt/cronjobs
+    echo "* * * * * cd /var/www/localhost/htdocs/ && php artisan schedule:run >> /dev/null 2>&1" >> /mnt/cronjobs
+    /usr/bin/crontab /mnt/cronjobs
+    echo "[i] CRON jobs initialized..."
+
+    echo "[i] MySQL data directory not found, creating initial DBs"
+
+    chown -R mysql:mysql /var/lib/mysql
+
+    # init database
+    echo '[i] MySQL initializing database...'
+    mysql_install_db --user=mysql > /dev/null
+    echo '[i] MySQL database initialized'
+
+    echo "[i] MySql root password: $MYSQL_PASSWORD"
+
+    # create temp file
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
+
+    # save sql
+    echo "[i] Create temp file: $tfile"
+    cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;
+EOF
+
+    # Create new database
+    echo "[i] Creating database: $MYSQL_DATABASE"
+    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+
+    # Set new user and password
+    dbpass=`pwgen -s -1 -v`
+
+    sed -i "s#DB_PASSWORD=123123#DB_PASSWORD=$dbpass#" /var/www/localhost/htdocs/.env
+
+    if [ "$MYSQL_USER" != "" ]; then
+        echo "[i] Creating user: $MYSQL_USER with password $dbpass"
+        echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$dbpass';" >> $tfile
+    fi
+
+    echo 'FLUSH PRIVILEGES;' >> $tfile
+
+    # run sql in tempfile
+    echo "[i] Run tempfile: $tfile"
+    /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
+
+    rm -f $tfile
+fi
+
+echo "[i] Starting all process"
+/usr/bin/mysqld --user=mysql --console &
+
+until mysqladmin ping &>/dev/null; do
+   echo -n "."; sleep 0.2
+done
+
+echo "[i] Executing migrations"
+/usr/bin/php /var/www/localhost/htdocs/artisan migrate
+
+exec /bin/sh "$@"
+

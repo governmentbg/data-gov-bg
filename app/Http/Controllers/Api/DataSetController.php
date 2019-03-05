4782f7cc265b9628944ac80d0621b32494abf13c
Reni Todorova <reni@finite-soft.com>
2019-03-05 13:33:28 +0200

    References #6378 Datasets and resources updated_by

--- app/Http/Controllers/Api/DataSetController.php
+++ app/Http/Controllers/Api/DataSetController.php
@@ -420,6 +420,8 @@ class DataSetController extends ApiController
                         $dataset->status = $post['data']['status'];
                     }
 
+                    $dataset->updated_by = Auth::id();
+                    
                     $dataset->save();
 
                     if (!empty($customFields)) {

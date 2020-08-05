2ea92307629c79f960489c457d1d40964414a476
Denis Tsatsarov <denis@finite-soft.com>
2020-08-05 13:02:55 +0300

    Fixes #8877 Fix social media buttons

--- resources/views/partials/social-icons.blade.php
+++ resources/views/partials/social-icons.blade.php
@@ -3,7 +3,7 @@
         <a target ="_blank" href="http://www.facebook.com/sharer.php?u={{ $shareUrl }}"><i class="fa fa-facebook"></i></a>
     </div>
     <div class="social tw">
-        <a target ="_blank" href="http://twitter.com/home?status={{ $shareUrl }}"><i class="fa fa-twitter"></i></a>
+        <a target ="_blank" href="http://twitter.com/intent/tweet?text={{ $shareUrl }}"><i class="fa fa-twitter"></i></a>
     </div>
     <div class="social in">
         <a target ="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url={{ $shareUrl }}"><i class="fa fa-linkedin"></i></a>

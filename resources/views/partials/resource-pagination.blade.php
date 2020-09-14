778561b6b45523434373c9e98bfd3a9461478053
Denis Tsatsarov <denis@finite-soft.com>
2020-09-14 17:05:20 +0300

    Refs #8910 Fix trusted checkbox, fix resources pagination info

--- resources/views/partials/resource-pagination.blade.php
+++ resources/views/partials/resource-pagination.blade.php
@@ -1,6 +1,19 @@
 @if (isset($resPagination))
     <div class="row">
-        <div class="col-xs-12 text-right">
+        <div class="col-xs-12 col-sm-4" style="padding-top: 30px;">
+            @if (
+                $resPagination instanceof Illuminate\Pagination\LengthAwarePaginator
+                && !empty($resPagination->items())
+            )
+                {{ sprintf(
+                    __('custom.resource_pagination_info'),
+                    $resPagination->items()['from'],
+                    $resPagination->items()['to'],
+                    $resPagination->total()
+                ) }}
+            @endif
+        </div>
+        <div class="col-xs-12 col-sm-8 text-right">
             {{ $resPagination->render() }}
         </div>
     </div>

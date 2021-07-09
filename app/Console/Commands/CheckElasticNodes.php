5fb52630f1b50bd44c88f86ae3b5f08bb7a29852
iliyan <iliyan.dimitrov@asap.bg>
2021-07-09 18:28:59 +0300

    OPENDATA-20

---
+++ app/Console/Commands/CheckElasticNodes.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\ElasticDataSet;
+use Elasticsearch as ES;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class CheckElasticNodes extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'elastic:nodes-check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check if all Elasticsearch nodes are running';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if(!ES::ping()) {
+          $this->error('Elasticsearch is not running');
+          die();
+        }
+
+        $nodes = ElasticDataSet::getElasticClusterParam('number_of_nodes');
+
+        if($nodes < 7) {
+
+          $this->info('Starting a check of Elasticsearch down nodes..');
+
+          $nodesIpsEnv = ElasticDataSet::getElasticHosts();
+          $runningNodesIps = ElasticDataSet::getElasticNodesIps();
+          $nodesDownIps = [];
+
+          foreach ($nodesIpsEnv as $ip) {
+            if(!in_array($ip, $runningNodesIps)) {
+
+              $this->line("");
+              $this->info("Node with ip $ip is down");
+
+              $nodesDownIps[] = $ip;
+            }
+          }
+
+          if(!empty($nodesDownIps)) {
+            $mailData = ['nodesDownIps' => $nodesDownIps];
+
+            $this->line("");
+            $this->info("Sending alert email");
+
+            Mail::send('mail/nodesDownAlert', $mailData, function ($m) {
+              $m->from(config('app.MAIL_FROM'), config('app.APP_NAME'));
+              $m->to('iliyan.dimitrov@asap.bg');
+              //$m->to('dev@asap.bg;skirov@e-gov.bg;bikozhuharov@e-gov.bg');
+              $m->subject(__('custom.cluster_nodes_down_alert'));
+            });
+          }
+        }
+
+        $this->line("");
+        $this->info("Check has finished");
+    }
+}
--- app/ElasticDataSet.php
+++ app/ElasticDataSet.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Elasticsearch as ES;
 
 class ElasticDataSet extends Model
 {
@@ -17,18 +18,138 @@ class ElasticDataSet extends Model
         $this->belongsTo('App\Resource');
     }
 
+    /**
+     * Get Elasticsearch cluster parameters
+     *
+     * @param string $param can be "cluster_name",
+     *                             "status",
+     *                             "timed_out",
+     *                             "number_of_nodes",
+     *                             "number_of_data_nodes",
+     *                             "active_primary_shards",
+     *                             "active_shards",
+     *                             "relocating_shards",
+     *                             "initializing_shards",
+     *                             "unassigned_shards",
+     *                             "delayed_unassigned_shards",
+     *                             "number_of_pending_tasks",
+     *                             "number_of_in_flight_fetch",
+     *                             "task_max_waiting_in_queue_millis",
+     *                             "active_shards_percent_as_number",
+     *                             "all"
+     * @return mixed
+     */
+    public static function getElasticClusterParam(string $param) {
+
+      if(ES::ping()) {
+        $clusterHealth = ES::cluster()->health();
+
+        return ($param == "all") ? $clusterHealth : $clusterHealth[$param];
+      }
+
+      return "No ping to ES";
+    }
+
+    /**\
+     * Get Elasticsearch cluster stats
+     *
+     * @return string
+     */
+    public static function getElasticClusterStats() {
+
+      if(ES::ping()) {
+        return ES::cluster()->stats();
+      }
+
+      return "No ping to ES";
+    }
+
+    /**
+     * Get Elasticsearch index info for docs
+     * @param $index
+     * @return mixed|string
+     */
+    public static function getElasticIndexInfo($index) {
+
+      if(ES::ping()) {
+        $indexStats = ES::indices()->stats(['index' => $index]);
+        if(isset($indexStats['indices'][$index]['primaries'])) {
+          return $indexStats['indices'][$index]['primaries'];
+        }
+      }
+
+      return "No ping to ES";
+    }
+
+    /**
+     * Get Elasticsearch index's docs count
+     * @param $index
+     * @return mixed|string
+     */
+    public static function getElasticIndexDocsCount($index) {
+
+      if(ES::ping()) {
+        $data = shell_exec('curl -X GET "'.env('ELASTICSEARCH_HOST').':'.env('ELASTICSEARCH_PORT').'/'.$index.'/_count"');
+
+        $dataArr = json_decode($data, true);
+        if(isset($dataArr['count'])) return $dataArr['count'];
+        else return 'N/A';
+      }
+
+      return "No ping to ES";
+    }
+
+    /**
+     * Get Elasticsearch cluster nodes ips
+     * @return mixed|array
+     */
+    public static function getElasticNodesIps() {
+
+      if(ES::ping()) {
+        $nodesIps = shell_exec('curl -X GET "'.env('ELASTICSEARCH_HOST').':'.env('ELASTICSEARCH_PORT').'/_cat/nodes?h=ip"');
+        $nodesIpsArr = explode("\n", $nodesIps);
+
+        if(empty($nodesIpsArr[count($nodesIpsArr)-1])) {
+          array_pop($nodesIpsArr);
+        }
+
+        return $nodesIpsArr;
+      }
+
+      return "No ping to ES";
+    }
+
     public static function getElasticData($id, $version)
     {
         $elasticData = ElasticDataSet::where('resource_id', $id)
             ->where('version', $version)
             ->first();
 
+        //dd(self::getElasticClusterStats());
+        //dd(self::getElasticIndexInfo('15036'));
+        //dd(self::getElasticClusterParam('number_of_nodes'));
+        //$nodesStats = ES::nodes()->stats();dd($nodesStats);
+        //$nodesInfo = ES::nodes()->info();dd($nodesInfo);
+        //$cluster = ES::cluster()->remoteInfo();dd($cluster);
+        //$snapshots = ES::snapshot()->getRepository();dd($snapshots);
+        //$cluster = ES::cluster()->health(['level' => 'indices']);dd($cluster);
+        //$indexSettings = ES::indices()->getSettings(['index' => '14425']);dd($indexSettings);
+        //$indexRecovery = ES::indices()->recovery(['index' => '15008', 'human' => true, 'detailed' => true]);dd($indexRecovery);
+        //$indexMapping = ES::indices()->putSettings(['index' => '14425', 'body' => ['number_of_replicas' => 0]]);dd($indexMapping);
+        //$indexMapping = ES::indices()->getMapping(['index' => '14425', 'type' => ElasticDataSet::ELASTIC_TYPE])['14425'];dd($indexMapping);
+        //$indexStats = ES::indices()->stats(['index' => '15008']);dd($indexStats);
+        //$esInfo = ES::info();dd($esInfo);
+        //$data = shell_exec('curl -X GET "172.23.116.75:9200/14425/_count"');dd(json_decode($data, true)['count']);
+        //$data = shell_exec('curl -X GET "172.23.116.75:9200/15036?pretty"');dd($data);
+        //$data = shell_exec('curl -X GET "172.23.116.75:9200/15036?pretty"');dd($data);
+
         if (!empty($elasticData)) {
-            $data = \Elasticsearch::get([
+            $data = ES::get([
                 'index' => $elasticData->index,
                 'type'  => $elasticData->index_type,
                 'id'    => $elasticData->doc,
             ]);
+            //dd($data);
         }
 
         $result = [];
@@ -65,5 +186,18 @@ class ElasticDataSet extends Model
 
             return $hosts;
         }
+    }
+
+    /**
+     * Get the Elasticsearch hosts from .env file
+     * @return false|string[]
+     */
+    public static function getElasticHosts()
+    {
+        if (getenv('ELASTICSEARCH_HOSTS')) {
+            return explode(',', env('ELASTICSEARCH_HOSTS'));
+        }
+
+        return null;
     }
 }
